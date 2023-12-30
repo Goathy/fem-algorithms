73840d7cbedf2484faac70aa53e5d622566fe94e
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-30 20:33:19 +0100

    Implement bubble sort... again

---
+++ src/bubble-sort.js
@@ -0,0 +1,23 @@
+import test from 'node:test'
+import assert from 'node:assert' 
+
+export function bubbleSort (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = tmp
+      }
+    }
+  }
+}
+
+test("bubble-sort", function () {
+    const arr = [9, 3, 7, 4, 69, 420, 42];
+
+    debugger;
+    bubbleSort(arr);
+    assert.deepEqual(arr, [3, 4, 7, 9, 42, 69, 420]);
+});
+
