7443b88df78ba870ec032609d6c77e6d43b165e9
Jakub Gałecki <goathy.dev@gmail.com>
2023-12-31 14:54:16 +0100

    Apply linter

--- src/bubble-sort.js
+++ src/bubble-sort.js
@@ -1,25 +1,23 @@
 'use strict'
 
 import test from 'node:test'
-import assert from 'node:assert' 
+import assert from 'node:assert'
 
 export function bubbleSort (arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        let tmp = arr[j]
-        arr[j] = arr[j+1]
-        arr[j+1] = tmp
+        const tmp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp
       }
     }
   }
 }
 
-test("bubble-sort", function () {
-    const arr = [9, 3, 7, 4, 69, 420, 42];
+test('bubble-sort', function () {
+  const arr = [9, 3, 7, 4, 69, 420, 42]
 
-    debugger;
-    bubbleSort(arr);
-    assert.deepEqual(arr, [3, 4, 7, 9, 42, 69, 420]);
-});
-
+  bubbleSort(arr)
+  assert.deepEqual(arr, [3, 4, 7, 9, 42, 69, 420])
+})
